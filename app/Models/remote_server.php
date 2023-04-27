9a380990faef36fc8d32926fb62f601f5536fecc
obel <obel.lasu@hologram.cd>
2023-04-27 10:38:50 +0100

    test

--- app/Models/remote_server.php
+++ app/Models/remote_server.php
@@ -18,7 +18,7 @@ class remote_server extends Model
     ];
 
 
-    public function GetData($url){
+    public function GetData($url,$token=""){
 
         $curl = curl_init();
 
@@ -30,9 +30,9 @@ class remote_server extends Model
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
                 'Content-Type: application/json',
-                
+                'token: '.$token,
+                'Authorization: Bearer '.$token,
             ),
         ));
         $response = curl_exec($curl);
@@ -41,6 +41,57 @@ class remote_server extends Model
 
         return $response;
     }
+
+
+    public function PostData($url,$dataArray){
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl,CURLOPT_HEADER, false); 
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/x-www-form-urlencoded'));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($dataArray));     
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        return $response;
+    }
+
+
+    public function RequestNewToken(){
+
+        $url = "https://getoken-dev.apps.kubedev.hologram.cd/Token";
+        $curl = curl_init();
+        curl_setopt_array(
+            $curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS =>'{
+                    "app_id": "frontend",
+                    "password": "swauuye820nsaiu"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json'
+                ),
+            )
+        );
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $response = json_decode($response);
+        $token = $response->content->accessToken;
+
+        return $token;
+       
+    }
+
 
 }
     
